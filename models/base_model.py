9935beae7db580a6cad797c26afcc291e6e88982
Camilo <trresqwe@gmail.com>
2020-11-03 10:09:10 -0500

    update base model

--- models/base_model.py
+++ models/base_model.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" """
+""" Base Model """
 
 
 import uuid
@@ -10,8 +10,10 @@ format_t = "%Y-%m-%dT%H:%M:%S.%f"
 
 
 class BaseModel():
-    """ that defines all common
-    attributes/methods for other classes"""
+    """
+    Defines all common
+    attributes/methods for other classes
+    """
 
     def __init__(self, *args, **kwargs):
         """ args: Not used"""
@@ -24,11 +26,12 @@ class BaseModel():
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+            self.updated_at = self.created_at
             models.storage.new(self)
 
     def __str__(self):
-        class_name = __class__.__name__
+        """ should print: [<class name>] (<self.id>) <self.__dict__>"""
+        class_name = self.__class__.__name__
         return ("[{}] ({}) {}".format(class_name, self.id, self.__dict__))
 
     def save(self):
@@ -37,7 +40,11 @@ class BaseModel():
         models.storage.save()
 
     def to_dict(self):
-        class_dict = self.__dict__
+        """
+        Returns a dictionary containing all keys/values
+        of __dict__ of the instance
+        """
+        class_dict = dict(self.__dict__)
         class_dict["__class__"] = self.__class__.__name__
         class_dict["created_at"] = self.created_at.isoformat()
         class_dict["updated_at"] = self.updated_at.isoformat()
