58f659c288c0c61a7fbcc4053480e57d0a1e5fa3
julianamonr03 <julianamonroy03@hotmail.com>
2020-11-04 23:56:18 +0000

    city test

---
+++ tests/test_models/test_city.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+'''test state'''
+
+import unittest
+import models
+import sys
+from models.city import City
+
+class test_CityModel(unittest.Testcase):
+
+    def save(self):
+        self.model = City()
+        self.model.save()
+
+    def test_instance_in_object(self):
+        self.assertTrue(hasattr(self.model, "state_id"))
+        self.assertTrue(hasattr(self.model, "name"))
+        self.assertEqual(self.model.state_id, "")
+        self.assertEqual(self.model.name, "")
+
+if __name__ == "__main__":
+    unittest.main()
