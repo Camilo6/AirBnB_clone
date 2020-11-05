2c64a8bf47292b1e38240cccb5a205ab47a89236
Camilo <trresqwe@gmail.com>
2020-11-05 18:38:52 -0500

    update

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -4,22 +4,20 @@ import unittest
 from models.review import Review
 
 
-class test_ReviewModel(unittest.Testcase):
+class test_Review(unittest.TestCase):
     """ Test review """
-
-    def save(self):
-        """test save"""
-        self.model = Review()
-        self.model.save()
+    def test_type_text(self):
+        """ Verify if is instance"""
+        self.assertIsInstance(Review.text, str)
 
     def test_instance_in_object(self):
         """test instance in objects"""
-        self.assertTrue(hasattr(self.model, "place_id"))
-        self.assertTrue(hasattr(self.model, "user_id"))
-        self.assertTrue(hasattr(self.model, "text"))
-        self.assertEqual(self.model.place_id, "")
-        self.assertEqual(self.model.user_id, "")
-        self.assertEqual(self.model.text, "")
+        self.assertTrue(hasattr(Review, "place_id"))
+        self.assertTrue(hasattr(Review, "user_id"))
+        self.assertTrue(hasattr(Review, "text"))
+        self.assertEqual(Review.place_id, "")
+        self.assertEqual(Review.user_id, "")
+        self.assertEqual(Review.text, "")
 
 if __name__ == "__main__":
     unittest.main()
