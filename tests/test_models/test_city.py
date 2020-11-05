2b24e79a8cd096cd32aef842bbe2c21238916ec7
Camilo <trresqwe@gmail.com>
2020-11-05 18:14:13 -0500

    update test city

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -5,22 +5,20 @@ import unittest
 import models
 import sys
 from models.city import City
+from models.base_model import BaseModel
 
-
-class test_CityModel(unittest.Testcase):
+class test_City(unittest.TestCase):
     """ Test city """
-
-    def save(self):
-        """test save"""
-        self.model = City()
-        self.model.save()
 
     def test_instance_in_object(self):
         """test instance in objects"""
-        self.assertTrue(hasattr(self.model, "state_id"))
-        self.assertTrue(hasattr(self.model, "name"))
-        self.assertEqual(self.model.state_id, "")
-        self.assertEqual(self.model.name, "")
+        self.assertTrue(hasattr(City, "state_id"))
+        self.assertTrue(hasattr(City, "name"))
+        self.assertEqual(City.state_id, "")
+        self.assertEqual(City.name, "")
+
+    def is_instace(self):
+        self.assertIsInstance(City, BaseModel)
 
 if __name__ == "__main__":
     unittest.main()
