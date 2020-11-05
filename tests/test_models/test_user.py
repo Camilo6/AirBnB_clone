66ccaa50e85b5da6f47ab1ee128150dfd5f1703d
Camilo <trresqwe@gmail.com>
2020-11-05 18:50:47 -0500

    Update test user

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -2,27 +2,29 @@
 """
 Unittest for User
 """
+
+
 import unittest
 from models.base_model import BaseModel
+from models.user import User
 
 
-class Test_attributes(unittest.TestCase):
+class test_User(unittest.TestCase):
     """ Tests for attributes """
-    def Test_id_type(self):
+
+    def test_name(self):
         """ test id """
-        self.assertEqual(type(self.id), str)
+        self.assertIsInstance(User.first_name, str)
 
-    def test_created_at(self):
-        """test created_at"""
-        save = BaseModel().to_dict()
-        self.assertEqual(type(self.created_at), datetime.datetime)
-        self.assertEqual(type(save.created_at), str)
-
-    def test_updated_at(self):
-        """test updated_at"""
-        save = BaseModel().to_dict()
-        self.assertEqual(type(self.updated_at), datetime.datetime)
-        self.assertEqual(type(save.updated_at), str)
+    def test_user(self):
+        self.assertTrue(hasattr(User, "email"))
+        self.assertTrue(hasattr(User, "password"))
+        self.assertTrue(hasattr(User, "first_name"))
+        self.assertTrue(hasattr(User, "last_name"))
+        self.assertEqual(User.email, "")
+        self.assertEqual(User.password, "")
+        self.assertEqual(User.first_name, "")
+        self.assertEqual(User.last_name, "")
 
 if __name__ == '__main__':
     unittest.main()
