90d192a2d1043c42427262ab062698063edcc3c4
julianamonr03 <julianamonroy03@hotmail.com>
2020-11-05 00:56:09 +0000

    update test

---
+++ tests/test_console.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""
+Unittest for BaseModel()
+"""
+import unittest
+
+
+class TestConsole(unittest.TestCase):
+    """test console"""
+
+    def test_console(self):
+        self.assertTrue(True)
