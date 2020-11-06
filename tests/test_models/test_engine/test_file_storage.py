453151b659b960334f7aa5e71de085bcfb64f0b2
Camilo <trresqwe@gmail.com>
2020-11-05 21:59:37 -0500

    update test file storage

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,34 +1,40 @@
 #!/usr/bin/python3
 
 """
-Unittest for BaseModel
+Unittest for FileStorage
 """
 
+import uuid
 import unittest
 from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
+from models.user import User
+
 
 
 class Test_attributes(unittest.TestCase):
     """ Tests attributes """
 
-    def test_id(self):
-        """ test id """
+    def test_new_data(self):
+        """ test new entry """
+        storage = FileStorage()
+        data = storage.all()
+        user = User()
+        user.id = str(uuid.uuid4())
+        user.name = "Juliana"
+        storage.new(user)
+        key = user.__class__.__name__ + "." + user.id
+        self.assertIsNotNone(data[key])
+    
+    #def test_created_at(self):
+        #""" test created_at """
+        #self.assertEqual(type(BaseModel.created_at), datetime.datetime)
+        #self.assertEqual(type(BaseModel.created_at), str)
 
-        self.assertEqual(type(self.id), str)
-
-    def test_created_at(self):
-        """ test created_at """
-
-        base = BaseModel().to_dict()
-        self.assertEqual(type(self.created_at), datetime.datetime)
-        self.assertEqual(type(base.created_at), str)
-
-    def test_updated_at(self):
-        """ test updated_at """
-
-        base = BaseModel().to_dict()
-        self.assertEqual(type(self.updated_at), datetime.datetime)
-        self.assertEqual(type(base.updated_at), str)
+    #def test_updated_at(self):
+    #    """ test updated_at """
+    #    self.assertEqual(type(self.updated_at), datetime.datetime)
+    #    self.assertEqual(type(BaseModel.updated_at), str)
 
 if __name__ == '__main__':
     unittest.main()
